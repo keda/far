1d40c8ee52936783942393a93fa89ac69f3ceda2
jinzhou.shao <happy_kaidy@163.com>
2014-07-08 18:06:08 +0800

    Config view with Thymeleaf framework.

--- src/main/java/com/sky/web/config/WebAppConfig.java
+++ src/main/java/com/sky/web/config/WebAppConfig.java
@@ -5,7 +5,10 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.thymeleaf.spring3.SpringTemplateEngine;
+import org.thymeleaf.spring3.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+import org.thymeleaf.templateresolver.TemplateResolver;
 
 @Configuration
 @EnableWebMvc
@@ -14,10 +17,30 @@ public class WebAppConfig {
 	
 	@Bean
 	ViewResolver viewResolver() {
-		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-        resolver.setPrefix("WEB-INF/views/");
-        resolver.setSuffix(".jsp");
+		ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+		resolver.setTemplateEngine(templateEngine());
+		resolver.setOrder(1);
+		resolver.setCharacterEncoding("UTF-8");
+		resolver.setViewNames(new String[]{"*"});
+		resolver.setCache(false);
         return resolver;
+	}
+	@Bean
+	TemplateResolver templateResolver() {
+		ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver();
+		templateResolver.setPrefix("/WEB-INF/views/");
+		templateResolver.setSuffix(".html");
+		templateResolver.setTemplateMode("HTML5");
+		templateResolver.setCharacterEncoding("UTF-8");
+		
+		templateResolver.setCacheable(false);
+		return templateResolver;
+	}
+	@Bean
+	SpringTemplateEngine templateEngine() {
+		SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+		templateEngine.setTemplateResolver(templateResolver());
+		return templateEngine;
 	}
 	
 }
